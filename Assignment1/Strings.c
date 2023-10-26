f8a479f04aa1ddcaf09cf74a3d985c827ccab2f5
SohanChepe <sohan.chepe@gmail.com>
2023-10-26 22:03:45 +0530

    loop added to prevent termination

--- Assignment1/Strings.c
+++ Assignment1/Strings.c
@@ -7,98 +7,109 @@ void copy(char[]);
 void reverse(char[]);
 
 int main()
-{
-    printf("IMPORTANT :-  String input should NOT contain any spaces. String length < 1000\n");
-    printf("OPTION MENU - \n");
-    printf("1. Substring (Checks if one string is a substring of the other.)\n");
-    printf("2. Palindrome (Checks if the given string is a palindrome. CASE SENSITIVE.)\n");
-    printf("3. Compare (Checks the equality of two strings)\n");
-    printf("4. Copy (Copies entered string into another string)\n");
-    printf("5. Reverse (Reverses the entered string)\n");
+{   
+    char cont = 'Y';
 
-    int input = 0;
-    printf("Select the operation that you want to perform :- ");
-    scanf("%d", &input);
-
-    switch (input)
+    while(cont == 'Y' || cont == 'y')
     {
-        case 1:
-            {
-                //Substring
-                char inparr1[1000];
-                char inparr2[1000];
-                printf("Enter primary string :- ");
-                scanf("%s", inparr1);
-                printf("Enter secondary string :- ");
-                scanf("%s", inparr2);
+        printf("IMPORTANT :-  String input should NOT contain any spaces. String length < 1000\n");
+        printf("OPTION MENU - \n");
+        printf("1. Substring (Checks if one string is a substring of the other.)\n");
+        printf("2. Palindrome (Checks if the given string is a palindrome. CASE SENSITIVE.)\n");
+        printf("3. Compare (Checks the equality of two strings)\n");
+        printf("4. Copy (Copies entered string into another string)\n");
+        printf("5. Reverse (Reverses the entered string)\n");
 
-                int answer = substring(inparr1, inparr2);
+        int input = 0;
+        printf("Select the operation that you want to perform :- ");
+        scanf("%d", &input);
 
-                if(answer == 1) printf("'%s' is a substring of '%s'\n", inparr2, inparr1);
-                else printf("'%s' is not a substring of '%s'\n", inparr2, inparr1);
-                break;
-            }
-        
-        case 2:
-            {
-                //Palindrome
-                char inparr1[1000];
-                printf("Enter string :- ");
-                scanf("%s", inparr1);
+        switch (input)
+        {
+            case 1:
+                {
+                    //Substring
+                    char inparr1[1000];
+                    char inparr2[1000];
+                    printf("Enter primary string :- ");
+                    scanf("%s", inparr1);
+                    printf("Enter secondary string :- ");
+                    scanf("%s", inparr2);
 
-                int answer = palindrome(inparr1);
+                    int answer = substring(inparr1, inparr2);
 
-                if(answer == 1) printf("'%s' is a palindrome\n", inparr1);
-                else printf("'%s' is not a palindrome\n", inparr1);
+                    if(answer == 1) printf("'%s' is a substring of '%s'\n", inparr2, inparr1);
+                    else printf("'%s' is not a substring of '%s'\n", inparr2, inparr1);
+                    break;
+                }
+            
+            case 2:
+                {
+                    //Palindrome
+                    char inparr1[1000];
+                    printf("Enter string :- ");
+                    scanf("%s", inparr1);
 
-                break;  
-            }
+                    int answer = palindrome(inparr1);
 
-        case 3:
-            {
-                //Compare
-                char inparr1[1000];
-                char inparr2[1000];
-                printf("Enter string 1 :- ");
-                scanf("%s", inparr1);
-                printf("Enter string 2 :- ");
-                scanf("%s", inparr2);
+                    if(answer == 1) printf("'%s' is a palindrome\n", inparr1);
+                    else printf("'%s' is not a palindrome\n", inparr1);
 
-                int answer = compare(inparr1, inparr2);
-                if(answer == 1) printf("'%s' is equal to '%s'\n", inparr2, inparr1);
-                else printf("'%s' is not equal to '%s'\n", inparr2, inparr1);
-                break;
-            }
-        
-        case 4:
-            {
-                //Copy
-                char inparr[1000];
-                printf("Enter a string :- ");
-                scanf("%s", inparr);
+                    break;  
+                }
 
-                copy(inparr);
+            case 3:
+                {
+                    //Compare
+                    char inparr1[1000];
+                    char inparr2[1000];
+                    printf("Enter string 1 :- ");
+                    scanf("%s", inparr1);
+                    printf("Enter string 2 :- ");
+                    scanf("%s", inparr2);
 
-                break;
-            }
+                    int answer = compare(inparr1, inparr2);
+                    if(answer == 1) printf("'%s' is equal to '%s'\n", inparr2, inparr1);
+                    else printf("'%s' is not equal to '%s'\n", inparr2, inparr1);
+                    break;
+                }
+            
+            case 4:
+                {
+                    //Copy
+                    char inparr[1000];
+                    printf("Enter a string :- ");
+                    scanf("%s", inparr);
 
-        case 5:
-            {
-                //Reverse
-                char inparr[1000];
-                printf("Enter a string :- ");
-                scanf("%s", inparr);
+                    copy(inparr);
 
-                reverse(inparr);
-                break;
-            }
-        
-        default:
-            {
-                printf("Enter a valid option.\n");
-                break;
-            }   
+                    break;
+                }
+
+            case 5:
+                {
+                    //Reverse
+                    char inparr[1000];
+                    printf("Enter a string :- ");
+                    scanf("%s", inparr);
+
+                    reverse(inparr);
+                    break;
+                }
+            
+            default:
+                {
+                    printf("Enter a valid option.\n");
+                    break;
+                }   
+        }
+
+        printf("\nDo you wish to continue? [Y/n]  ");
+        scanf("%s", &cont);
+        printf("\n");
     }
+
+    return 0;
 }
 
 int substring(char arr1[], char arr2[])
